572246f9b7501a642daa7f1ecab8587259027a7f
thanhhung0112 <thanhhungthh987@gmail.com>
2022-11-11 02:50:22 +0700

    update

--- my_api.py
+++ my_api.py
@@ -6,6 +6,7 @@ import os
 from random import random
 # import core
 # import serial
+import time 
 import camera
 import detect
 
@@ -18,45 +19,46 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['UPLOAD_FOLDER'] = 'static'
 
 # The Specifications of clock
-min_angle = 20
-max_angle = 344
+min_angle = 39
+max_angle = 316 
 min_value = 0
-max_value = 150
+max_value = 100
 loop = 8
-url = "http://192.168.1.5:8080/shot.jpg"
+url = "http://192.168.0.191:8080/shot.jpg"
 
 @app.route('/', methods=['POST', 'GET'])
 @cross_origin(origin='*')
 def detect_temperature():
-    # if request.method == "POST":
-    try:
-        # image = request.files['file']
-        image = camera.get_image(url, loop)
-        loop += 1
-        if isinstance(image, type(None)):
-        # if 1:
-            # Lưu file
-            # path_to_save = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
-            path_to_save = os.path.join(app.config['UPLOAD_FOLDER'], f'test{loop}.png')
+    if request.method == "POST":
+        try:
+            # image = request.files['file']
+            image = camera.get_frame(url, loop)
 
-            print("Save = ", path_to_save)
-            # image.save(path_to_save)
+            if not(isinstance(image, type(None))):
+            # if 1:
+                # Lưu file
+                # path_to_save = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
+                path_to_save = os.path.join(app.config['UPLOAD_FOLDER'], f'test{loop}.png')
 
-            x, y, r, circle = detect.calibrate_gauge(image)
+                print("Save = ", path_to_save)
+                # image.save(path_to_save)
 
-            res, img = detect.get_current_value(image, circle, min_angle, max_angle, min_value, max_value, x, y, r)
+                x, y, r, circle = detect.calibrate_gauge(image)
 
-            cv.imwrite(path_to_save, img)
-            # return render_template('index.html', user_image=image.filename, rand=str(random()), msg='Success', res=res)
-            return render_template('index.html', user_image=f'test{loop}.png', rand=str(random()), msg='Success', res=res)
-        else:
-            # Nếu không có file thì yêu cầu tải file
-            return render_template('index.html', msg='Không nhận diện được nhiệt độ')
-    except:
-        return render_template('index.html', msg='Không kết nối được với camera')
+                res, img = detect.get_current_value(image, circle, min_angle, max_angle, min_value, max_value, x, y, r)
 
-    # else:
-    #     return render_template('index.html')
+                cv.imwrite(path_to_save, img)
+                # return render_template('index.html', user_image=image.filename, rand=str(random()), msg='Success', res=res)
+                return render_template('index.html', user_image=f'test{loop}.png', rand=str(random()), msg='Success', res=res)
+            else:
+                # Nếu không có file thì yêu cầu tải file
+                return render_template('index.html', msg='Không nhận diện được nhiệt độ')
+        except:
+            print("something's wrong, fix bug")
+            return render_template('index.html', msg='Không kết nối được với camera')
+
+    else:
+        return render_template('index.html')
 
 # Start backend
 if __name__ == '__main__':
